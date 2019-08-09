7d530229dd5cc8290d4cf88129a0d2cd9faf4d6e
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-08-09 21:17:00 +0200

    add assert

--- src/app/views/dashboard/asserts/add-assert/add-assert.component.ts
+++ src/app/views/dashboard/asserts/add-assert/add-assert.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SmsService } from 'src/app/_services/dashboard/sms.service';
+import { MessageService } from 'primeng/api';
+import { AssertService } from 'src/app/_services/dashboard/assert.service';
+import { CategoryService } from 'src/app/_services/dashboard/category.service';
 
 @Component({
   selector: 'app-add-assert',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAssertComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private routeTo: Router,
+    private route: ActivatedRoute,
+    private smsService: SmsService,
+    private messageService: MessageService,
+    private assertService: AssertService,
+    private categoryService: CategoryService
+  ) {
+
+  }
 
   ngOnInit() {
+    this.rForm = this.fb.group({
+      CategoryId: [null, Validators.required],
+      Category: [null, Validators.required],
+      Description: [null, Validators.required]
+    });
+  }
+
+  saveAssert(model) {
+    this.smsService.send(model).subscribe(response => {
+      // alert(JSON.stringify(response));
+      this.messageService.add({ severity: 'success', summary: 'Success Message', detail: 'Order submitted' });
+    });
   }
 
 }
