0c56eec25ee12c52651ddcea61e8543adefc8bfb
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-08-23 07:36:36 +0200

    slider

--- src/app/views/home/index/Testimonials/Testimonials.component.ts
+++ src/app/views/home/index/Testimonials/Testimonials.component.ts
@@ -6,10 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
-
+  showOne = true;
+  showTwo: boolean;
+  showThree: boolean;
   constructor() { }
 
   ngOnInit() {
   }
+  next(code: number) {
+    if (code === 1) {
+      if (this.showOne) {
+        this.showOne = false;
+        this.showTwo = true;
+        this.showThree = false;
+        return;
+      }
 
+      if (this.showTwo) {
+        this.showOne = false;
+        this.showTwo = false;
+        this.showThree = true;
+        return;
+
+      }
+
+      if (this.showThree) {
+        this.showOne = true;
+        this.showTwo = false;
+        this.showThree = false;
+        return;
+
+      }
+    } else {
+      if (this.showOne) {
+        this.showOne = false;
+        this.showTwo = false;
+        this.showThree = true;
+        return;
+
+      }
+
+      if (this.showTwo) {
+        this.showOne = true;
+        this.showTwo = false;
+        this.showThree = false;
+        return;
+
+      }
+
+      if (this.showThree) {
+        this.showOne = false;
+        this.showTwo = true;
+        this.showThree = false;
+        return;
+
+      }
+    }
+  }
 }
