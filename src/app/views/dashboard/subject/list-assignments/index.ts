c843e81ef05d020a04c16dfee8483ac10520750e
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-12-13 13:22:53 +0200

    Lessons list and mobile responsiveness. Deploy latest changes Create Subjects.

---
+++ src/app/views/dashboard/subject/list-assignments/index.ts
@@ -0,0 +1,4 @@
+// start:ng42.barrel
+export * from './list-assignments.component';
+// end:ng42.barrel
+
---
+++ src/app/views/dashboard/subject/subjects/index.ts
@@ -0,0 +1,4 @@
+// start:ng42.barrel
+export * from './subjects.component';
+// end:ng42.barrel
+
